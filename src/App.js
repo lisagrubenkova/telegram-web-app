a8a59a72dd8f7253adc236583ed27c018e4a346d
Elizaveta <elizaveta.grubenkova@gmail.com>
2025-01-10 20:16:59 +0300

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useTelegram } from './hooks/useTelegram';
 
 
 function App() {
-  const {tg, onToggleButton} = useTelegram()
+  const {onToggleButton, tg} = useTelegram()
 
   useEffect(() => {
     tg.ready()
