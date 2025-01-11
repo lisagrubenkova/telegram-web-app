be3e32d94c93e4278fc60b0061abc6c860bc7117
Elizaveta <elizaveta.grubenkova@gmail.com>
2025-01-11 22:34:42 +0300

    bugs fixed

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,7 +12,7 @@ const Form = () => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
         })
-    }, [tg.MainButton])
+    })
 
     useEffect(() => {
         if (!street || !country) {
